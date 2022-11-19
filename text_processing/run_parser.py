8dd00f7b3b206fca711e4c1b6391ba2ffd123af1
Kseniya.Edkova <Kseniya.Edkova@tatar.ru>
2022-11-19 15:38:05 +0300

    parsing: songs to json (test+full)

---
+++ text_processing/run_parser.py
@@ -0,0 +1,6 @@
+# Первый запуск - для добавления ссылок на все песни в файл all_songs.txt
+# Второй запуск - первер всех песен и запись в формате json в файл songs_info.txt
+import parser_be
+pars = parser_be.ParsBeesona(True)
+pars.run_parser()
+
